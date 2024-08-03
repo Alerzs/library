b4165927a0525eb2b26c54f5e1950fbc7590fb32
Hamidreza <hamidreza.mugen@gmail.com>
2024-08-03 17:29:17 +0330

    bookmanager

--- template.py
+++ template.py
@@ -26,56 +26,74 @@ class Book:
         
 
 class BookManager:
-
-    def __init__(self) -> None:
+    def init(self) -> None:
         try:
             with open('books_data.pkl', 'rb') as inp:
                 data = pickle.load(inp)
-                inp.close()
                 self.books_list = data
         except:
             self.books_list = []
-    
+
     def save_book(self):
         with open('books_data.pkl', 'wb') as otp:
-            pickle.dump(self.books_list , otp)
+            pickle.dump(self.books_list, otp)
 
+    def add_new_book(self, name, author, genre, quantity):
+        for book in self.books_list:
+            if book.get_name() == name:
+                print("This book already exists.")
+                return
+        new_book = Book(name, author, genre, quantity)
+        self.books_list.append(new_book)
+        self.save_book()
+        print("New book added!")
 
-    def add_new_book(self , name ,author , genre , quantity):
-        #adds new book and append in books_list
-        pass
+    def search_book_name(self, name):
+        for book in self.books_list:
+            if book.get_name() == name:
+                return book
+        return
 
-    def search_book_name(self , name):
-        #iterates in books_list whenever its name is equal to input name returns the book object otherwise returns false
-        pass
+    def search_book_author(self, author):
+        result = []
+        for book in self.books_list:
+            if book.get_author() == author:
+                result.append(book)
+        if result:
+            return result
+        else:
+            return
 
-    def search_book_author(self , author):
-        #like previous method but this time returns a list of book object that has the given author
-        pass
-
-    def search_book_genre(self , genre):
-        #similar to author search
-        pass
+    def search_book_genre(self, genre):
+        result = []
+        for book in self.books_list:
+            if book.get_genre() == genre:
+                result.append(book)
+        if result:
+            return result
+        else:
+            return
 
     def search_menu(self):
-
-        print("press 'A' to search by author")
-        print("press 'N' to search by books name")
-        print("press 'G' to search by genre")
+        print("Press 'A' to search by author")
+        print("Press 'N' to search by book's name")
+        print("Press 'G' to search by genre")
         inp = input().upper()
 
         if inp == "A":
-            author = input("please enter author name")
+            author = input("Please enter author name: ")
             return self.search_book_author(author)
         elif inp == "N":
-            name = input("please enter book name")
+            name = input("Please enter book name: ")
             return self.search_book_name(name)
         elif inp == "G":
-            genre = input("please enter genre")
+            genre = input("Please enter genre: ")
             return self.search_book_genre(genre)
         else:
-            print("wrong option")
-            self.search_menu()
+            print("Wrong option")
+            return self.search_menu()
+
+
 
 class Member:
 
